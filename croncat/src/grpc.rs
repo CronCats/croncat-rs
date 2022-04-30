723e9aa4a6486e8f45c5a8a3b06ef576e696d013
Zack Kollar <rallokkcaz@gmail.com>
2022-04-29 22:09:49 -0700

    :notes: Add environment variables loading

--- croncat/src/grpc.rs
+++ croncat/src/grpc.rs
@@ -9,11 +9,9 @@ use crate::errors::Report;
 /// Create message and query clients for interacting with the chain.
 /// 
 #[no_coverage]
-pub async fn connect() -> Result<(MsgClient<Channel>, QueryClient<Channel>), Report> {
-  let url = "http://[::1]:50051";
-
-  let msg_client = MsgClient::connect(url).await?;
-  let query_client = QueryClient::connect(url).await?;
+pub async fn connect(url: &String) -> Result<(MsgClient<Channel>, QueryClient<Channel>), Report> {
+  let msg_client = MsgClient::connect(url.clone()).await?;
+  let query_client = QueryClient::connect(url.clone()).await?;
 
   info!("Connected to GRPC services @ {}", url);
   

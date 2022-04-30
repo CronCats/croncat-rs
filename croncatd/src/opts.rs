50189c9cdc93fede5835680f9cad3c53d3f9c660
Zack Kollar <rallokkcaz@gmail.com>
2022-04-29 19:11:46 -0700

    :notes: Scaffold the CLI program layout and tokio tasks

---
+++ croncatd/src/opts.rs
@@ -0,0 +1,16 @@
+use structopt::StructOpt;
+
+///
+/// Command line options.
+/// 
+#[derive(Debug, StructOpt)]
+#[structopt(name = "example", about = "An example of StructOpt usage.")]
+pub struct Opts {
+    /// Activate debug mode
+    #[structopt(short, long)]
+    pub debug: bool,
+
+    /// Wether to show the banner or not
+    #[structopt(short, long)]
+    pub no_banner: bool,
+}

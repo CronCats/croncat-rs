e3bab7dd75e8bc4475f1424d676813705ae82c02
Zack Kollar <rallokkcaz@gmail.com>
2022-05-01 01:52:46 -0700

    :notes: Abandon struct abstraction for now, keep with the module pattern

--- croncat/src/channels.rs
+++ croncat/src/channels.rs
@@ -2,14 +2,16 @@
 //! Various channel types for croncat.
 //!
 
-use tokio::sync::watch;
+use tokio::sync::{mpsc, watch};
 
-///
 /// Shutdown channel Sender.
-///
 pub type ShutdownTx = watch::Sender<()>;
 
-///
 /// Shutdown channel Receiver.
-///
 pub type ShutdownRx = watch::Receiver<()>;
+
+/// Block stream channel Sender.
+pub type BlockStreamTx = mpsc::Sender<tendermint::Block>;
+
+/// Block stream channel Receiver.
+pub type BlockStreamRx = mpsc::Receiver<tendermint::Block>;
--- croncat/src/consumers/block_consumer.rs
+++ croncat/src/consumers/block_consumer.rs
@@ -1,1 +1,9 @@
-pub fn process() {}
+use crate::{channels::BlockStreamRx, errors::Report, logging::info};
+
+pub async fn consume(block_stream_rx: &mut BlockStreamRx) -> Result<(), Report> {
+    while let Some(block) = block_stream_rx.recv().await {
+        info!("{:#?}", block.header.time);
+    }
+
+    Ok(())
+}

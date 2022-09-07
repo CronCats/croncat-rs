eacdf6849f28914b94dde857162a7784838c3cad
Deveus <raffree.dev@gmail.com>
2022-09-07 18:56:50 +0300

    feat: ChainRegistry, config override, network type configs and many more...

--- croncatd/src/main.rs
+++ croncatd/src/main.rs
@@ -6,7 +6,7 @@ use std::process::exit;
 
 use croncat::{
     channels,
-    config::ChainConfig,
+    config::{ChainConfig, NetworkType},
     env,
     errors::Report,
     grpc::{GrpcQuerier, GrpcSigner},
@@ -26,6 +26,7 @@ mod opts;
 
 #[tokio::main]
 async fn main() -> Result<(), Report> {
+    
     // Get environment variables
     let env = env::load()?;
     let mut storage = LocalAgentStorage::new();
@@ -45,6 +46,7 @@ async fn main() -> Result<(), Report> {
     if !opts.no_frills {
         cli::print_banner();
     }
+    let network_type:Option<NetworkType>=Some(NetworkType::Testnet);
     info!("Starting croncatd...");
     match opts.cmd {
         opts::Command::RegisterAgent {
@@ -52,7 +54,7 @@ async fn main() -> Result<(), Report> {
             sender_name,
         } => {
             let key = storage.get_agent_signing_key(&sender_name)?;
-            let signer = GrpcSigner::new(ChainConfig::new()?, key, env.croncat_addr).await?;
+            let signer = GrpcSigner::new(ChainConfig::new(network_type).await?, key, env.croncat_addr).await?;
 
             println!("Key: {}", signer.key().public_key().to_json());
             println!("Payable account Id {:?}", payable_account_id);
@@ -63,35 +65,35 @@ async fn main() -> Result<(), Report> {
         }
         opts::Command::UnregisterAgent { sender_name } => {
             let key = storage.get_agent_signing_key(&sender_name)?;
-            let signer = GrpcSigner::new(ChainConfig::new()?, key, env.croncat_addr).await?;
+            let signer = GrpcSigner::new(ChainConfig::new(network_type).await?, key, env.croncat_addr).await?;
             let result = signer.unregister_agent().await?;
             let log = result.log;
             println!("{log}");
         }
         opts::Command::Withdraw { sender_name } => {
             let key = storage.get_agent_signing_key(&sender_name)?;
-            let signer = GrpcSigner::new(ChainConfig::new()?, key, env.croncat_addr).await?;
+            let signer = GrpcSigner::new(ChainConfig::new(network_type).await?, key, env.croncat_addr).await?;
             let result = signer.withdraw_reward().await?;
             let log = result.log;
             println!("{log}");
         }
         opts::Command::Info => {
-            let querier = GrpcQuerier::new(&env.croncat_addr, &ChainConfig::new()?).await?;
+            let querier = GrpcQuerier::new(&env.croncat_addr, &ChainConfig::new(network_type).await?).await?;
             let config = querier.query_config().await?;
             println!("{config}")
         }
         opts::Command::GetAgentStatus { account_id } => {
-            let querier = GrpcQuerier::new(&env.croncat_addr, &ChainConfig::new()?).await?;
+            let querier = GrpcQuerier::new(&env.croncat_addr, &ChainConfig::new(network_type).await?).await?;
             let status = querier.get_agent(account_id).await?;
             println!("{status}")
         }
         opts::Command::Tasks { from_index, limit } => {
-            let querier = GrpcQuerier::new(&env.croncat_addr, &ChainConfig::new()?).await?;
+            let querier = GrpcQuerier::new(&env.croncat_addr, &ChainConfig::new(network_type).await?).await?;
             let tasks = querier.get_tasks(from_index, limit).await?;
             println!("{tasks}")
         }
         opts::Command::GetAgentTasks { account_addr } => {
-            let querier = GrpcQuerier::new(&env.croncat_addr, &ChainConfig::new()?).await?;
+            let querier = GrpcQuerier::new(&env.croncat_addr, &ChainConfig::new(network_type).await?).await?;
             let agent_tasks = querier.get_agent_tasks(account_addr).await?;
             println!("{agent_tasks}")
         }
@@ -101,7 +103,7 @@ async fn main() -> Result<(), Report> {
             sender_name,
         } => {
             let key = storage.get_agent_signing_key(&sender_name)?;
-            let signer = GrpcSigner::new(ChainConfig::new()?, key, env.croncat_addr).await?;
+            let signer = GrpcSigner::new(ChainConfig::new(network_type).await?, key, env.croncat_addr).await?;
             let result = signer.update_agent(payable_account_id).await?;
             let log = result.log;
             println!("{log}");
@@ -113,7 +115,7 @@ async fn main() -> Result<(), Report> {
         opts::Command::GetAgent { name } => storage.display_account(&name),
         opts::Command::Go { sender_name } => {
             let key = storage.get_agent_signing_key(&sender_name)?;
-            let signer = GrpcSigner::new(ChainConfig::new()?, key, &env.croncat_addr).await?;
+            let signer = GrpcSigner::new(ChainConfig::new(network_type).await?, key, &env.croncat_addr).await?;
             let (shutdown_tx, shutdown_rx) = channels::create_shutdown_channel();
             system::go(env, shutdown_tx, shutdown_rx, signer).await?;
         }
